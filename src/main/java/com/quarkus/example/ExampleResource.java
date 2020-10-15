8d0cb28c515637fb5c6678de27c26cd7806a239a
HarshadRanganathan <rharshad93@gmail.com>
2020-10-15 16:21:15 +0100

    Use config properties

--- src/main/java/com/quarkus/example/ExampleResource.java
+++ src/main/java/com/quarkus/example/ExampleResource.java
@@ -1,5 +1,7 @@
 package com.quarkus.example;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/resteasy/hello")
 public class ExampleResource {
 
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return greeting;
     }
 }
